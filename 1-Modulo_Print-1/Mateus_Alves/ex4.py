539b1fdf977303bf203367ff1b3a7cd0b4cb6670
Mateus-Silva11 <alvesdasilva1106@gmail.com>
2019-11-22 11:39:50 -0300

    Exercicos Resolvidos Função Print('<3')

--- 1-Modulo_Print-1/Mateus_Alves/ex4.py
+++ 1-Modulo_Print-1/Mateus_Alves/ex4.py
@@ -6,3 +6,11 @@
 #---   2 - Listar bebidas não alcoolicas
 #---   3 - Visualizar Pedido
 #---   4 - Sair
+
+print('='*50, '\n')
+print("1 - Listar bebidas alcoolicas")
+print('2 - Listar bebidas não alcoolicas')
+print('3 - Visualizar Pedido')
+print('4 - Sair')
+print('\n')
+print('='*50)
