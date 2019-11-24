f598054ab09093fea52f4ce49004dbe98c454736
Matheus Abreu <abreumatheus@icloud.com>
2019-11-24 21:18:05 -0200

    Resolução Módulo 16 / Ex 1

--- 16-Modulo_Funcao-1/ex1.py
+++ 16-Modulo_Funcao-1/ex1.py
@@ -3,3 +3,14 @@
 #--- O cabeçalho deve ser escrito usando a multiplicação de carácter
 #--- O cabeçalho deev conter o nome de uma empresa, que será uma variável
 #--- Realize a chamada da função na ultima linha do seu programa
+
+def header(empresa):
+    empresa = empresa
+    print('#'*55, '\n')
+    print(empresa)
+    print('\n', '#'*55, sep='')
+    return ''
+
+empresa = 'HBSIS / Anheuser Busch InBev'
+
+print(header(empresa))
