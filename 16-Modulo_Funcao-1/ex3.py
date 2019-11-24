fa500debeefa93f95b2c12bd45128ba3330d373d
Matheus Abreu <abreumatheus@icloud.com>
2019-11-24 21:29:50 -0200

    Resolução Módulo 16 / Ex 3

--- 16-Modulo_Funcao-1/ex3.py
+++ 16-Modulo_Funcao-1/ex3.py
@@ -4,3 +4,12 @@
 #--- Calcule a média entre os três números e armazene em uma quarta variável
 #--- Imprima a média e uma mensagem usando f-string (módulo 3)
 #--- Deve ser impresso apenas duas cadas após a vírgula
+
+def media():
+    n1 = float(input('\nDigite o primeiro número: '))
+    n2 = float(input('Digite o segundo número: '))
+    n3 = float(input('Digite o terceiro número: '))
+    result_media = (n1 + n2 + n3)/3
+    return print(f'\nO resultado da média entre {n1}, {n2} e {n3} é: {result_media:.2f}')
+
+media()
