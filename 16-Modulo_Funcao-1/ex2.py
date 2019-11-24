c8718e84a19439fc116189bd57e123b321d2aa74
Matheus Abreu <abreumatheus@icloud.com>
2019-11-24 21:24:06 -0200

    Resolução Módeulo 16 / Ex 2

--- 16-Modulo_Funcao-1/ex2.py
+++ 16-Modulo_Funcao-1/ex2.py
@@ -3,3 +3,11 @@
 #--- Armazene cada número em uma variável
 #--- Realize a soma entre os dois números e armazene o resultado em uma terceira variável
 #--- Imprima o resultado e uma mensagem usando f-string (módulo 3)
+
+def soma():
+    n1 = float(input('\nDigite o primeiro número: '))
+    n2 = float(input('Digite o segundo número: '))
+    result_soma = n1 + n2
+    return print(f'\nO reseultada da soma dos números é: {result_soma}')
+
+soma()
