872fe3f62b26634fcdaeafd9a5b837303b78f53c
Marciano <marciano.et@hotmail.com>
2019-11-24 18:57:42 -0300

    resolução

--- 1-Modulo_Print-1/Marciano_Pazinatto/ex1.py
+++ 1-Modulo_Print-1/Marciano_Pazinatto/ex1.py
@@ -1,3 +1,9 @@
 #--- Exercício 1 - Impressão de dados com a função Print
 #--- Imprima textos com seu nome, sobrenome e idade 
 #--- Cada informação deve ser impressa em uma linha diferente 
+n='Marciano'
+s='Pazinatto'
+i='31'
+print('\n'*2)
+print(f'Meu nome é {n}\n\tMeu sobrenome é {s}\n\t\tE tenho {i} anos de idade')
+print('\n'*2)
--- 1-Modulo_Print-1/Marciano_Pazinatto/ex5.py
+++ 1-Modulo_Print-1/Marciano_Pazinatto/ex5.py
@@ -7,3 +7,19 @@
 #--- 1111111111  111111111
 #--- 11      11  11      11
 #--- 11      11  111111111
+
+print('''
+MMMMMMMMM                                  MMM        MMM       MMMMMMMMMM          MMMMMMMMM      MMMMMMM      MMMMMMMMM
+MMMMMMMMMMMMMM                             MMM        MMM       MMM      MMM      MMMMMMMMMMMMM      MMM      MMMMMMMMMMMMM
+MMMMMMMMM   MMMMMMM                        MMM        MMM       MMM       MMM     MMM        MM      MMM      MMM        MM
+MMMMMMM      MMMMMMMMMM                    MMM        MMM       MMM       MMM     MMM                MMM      MMM
+MMMMMM        MMMMMMMMMMMMMMMM             MMM        MMM       MMM      MMM      MMM                MMM      MMM
+MMMMM          MMMMMMMMMMMMMMMMMMMMM       MMMMMMMMMMMMMM       MMMMMMMMMM        MMMMMMMMMM         MMM      MMMMMMMMMM
+MMMM             MMMMMMMMMMMMMMMMMMMM      MMMMMMMMMMMMMM       MMMMMMMMMM           MMMMMMMMM       MMM        MMMMMMMMMMM
+MMM               MMMMMMMMMMMMMMM          MMM        MMM       MMM      MMM               MMM       MMM                MMM
+MMMMMMMMMMMMMMMMMMMMMMMMM                  MMM        MMM       MMM       MMM              MMM       MMM                MMM
+MMMMMMMMMMMMMMMMMMM                        MMM        MMM       MMM       MMM     MM       MMM       MMM      MM        MMM
+MMMMMMMMMMMMMMM                            MMM        MMM       MMM      MMM      MMMMMMMMMMMM       MMM      MMMMMMMMMMMMM
+MMMMMMMMMM                                 MMM        MMM       MMMMMMMMMM         MMMMMMMMM       MMMMMMM     MMMMMMMMMM
+
+''')
