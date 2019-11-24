dfda444734fce806cc4c35162f7f66d638d9ad73
Maykon D. Granemann <maykondgranemann@gmail.com>
2019-11-24 17:13:14 -0300

    Movido para Lista 2

--- Abioluz_Exercicios/Exercicios_Metodos/ex1.py
+++
@@ -1,5 +0,0 @@
-# Crie um programa que leia 5 valores e retorne a média dele.
-# Este programa não pode usar o len() e nem o sum() para fazer a média
-# O código do método/função não podem ter import
-# NÃO PODE EXCEDER A 15 LINHAS DE CÓGIGO!
-
--- Abioluz_Exercicios/Exercicios_Metodos/ex2.py
+++
@@ -1,7 +0,0 @@
-# Crie um programa para cálculo de média para estas duas listas: 
-# Faturamento mês de Agosto/2019 por vendedor: 4585.05, 7512.68, 8658.78, 5003.98
-# Quantidade de garrafas de cerveja (por cmomércio) vendidos na Area 1 - Blumenau - Novembro/2019: 5096, 10058, 4001, 9750, 15168, 7859, 500
-# O cálculo de média não pode usar as funções sum() e nem len()
-# será necessário um código enxuto de no máximo 7 linhas
-# Os resultados deverão ser apresentados por linha
-
