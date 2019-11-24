9b6e4903b0cb11729afd9f95b8a61f2950937f6a
ElementaryBR <juniorcardoso.sc@outlook.com>
2019-11-24 13:52:04 -0200

    Resolução de alguns exercicios

--- 1-Modulo_Print-1/ex2.py
+++ 1-Modulo_Print-1/ex2.py
@@ -4,3 +4,15 @@
 #--- O menu deve conter as opções de Cadastrar, Alterar, Listar e Sair
 #--- Cada opção deve vir acompanhada de um número
 #--- Cada opção deve ser impressa em uma linha diferente
+
+print('@'*20, 'Cadastramento de Pessoas', '@'*20)
+print('\nCadastre seu nome completo aqui:')
+print('\nQual a sua idade?: ')
+print('\nDigite seu genero: ')
+print('\nDigite seu CPF: ')
+print('\nDigite seu RG: ')
+print('\nQual a sua escolaridade?:')
+print('\nQual o seu tipo sanguineo')
+print('\n Deseja alterar algum Cadastro? [S/N]:')
+print('\n Deseja visualizar algum dado informado para correção?\n Digite qual dado:(Nome,Idade,Sexo...)')
+print('\n','\t','@'*20, 'Cadastramento de Pessoas', '@'*20)
