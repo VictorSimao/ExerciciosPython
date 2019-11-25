07bffaf821e6d24da0dae6f8df5ce46e3f31f613
Matheus Abreu <abreumatheus@icloud.com>
2019-11-24 22:04:51 -0200

    Resolução Módulo 18 / Ex 2

--- 18-Modulo_Funcao-2/ex2.py
+++ 18-Modulo_Funcao-2/ex2.py
@@ -7,3 +7,12 @@
 #--- Realize a leitura da base e da potência fora da função 
 #--- A leitura deve ser feita através do terminal e atribuída à duas variáveis
 #--- Realize a chamada da função passando a base e a potência
+
+def pot(base, expo):
+    result_pot = base**expo
+    return print('\nO resultado da potência com base {} e expoente {} é: {}'.format(base, expo, result_pot))
+
+base = int(input('\nDigite a base da potência: '))
+expo = int(input('Digite o expoente: '))
+
+pot(base, expo)
