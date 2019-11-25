378ac41041a294ac09219e796401d38d944144b9
Matheus Abreu <abreumatheus@icloud.com>
2019-11-25 08:27:48 -0300

    Update Módulo 3

--- 3-Modulo_Print-2/ex4.py
+++ 3-Modulo_Print-2/ex4.py
@@ -1,1 +1,8 @@
 #--- Exercício 4  - Print -2
+#--- Crie uma variável e atribua uma string com o seu nome
+#--- Crie uma variável de nome linkedin e atribua um valor booleano 
+#--- Crie uma variável de nome instagran e atribua um valor booleano 
+#--- Crie uma variável de nome facebook e atribua um valor booleano 
+#--- Crie uma variável de nome telegram e atribua um valor booleano 
+#--- Imprima as cinco variáveis juntamente com uma frase dizendo se possui conta nas mídias citadas 
+#--- Para impressão utilize f-string
