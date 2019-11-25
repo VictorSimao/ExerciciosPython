d67ef6ca63b4b3198dd0aa100e0db0419eaa5085
Matheus Abreu <abreumatheus@icloud.com>
2019-11-25 08:27:09 -0300

    Resolução Módulo 2 / Ex 1 e 2

--- 2-Modulo_Variaveis-1/ex1.py
+++ 2-Modulo_Variaveis-1/ex1.py
@@ -4,3 +4,10 @@
 #--- Deve ser usado apenas uma vez a função print()
 #--- Cada dado deve ser impresso em uma linha diferente
 #--- O salário deve ser de tipo flutuante
+nome = input('Digite o nome do funcionário: ')
+sobrenome = input('Digite o sobrenome do funcionário: ')
+cpf = input('Digite o CPF do funcionário: ')
+rg = input('Digite o RG do funcionário: ')
+salario = float(input('Digite o salário do funcionário: '))
+
+print(f'Nome: {nome} \nSobrenome: {sobrenome} \nCPF: {cpf} \nRG: {rg} \nSalário: {salario:.2f}')
