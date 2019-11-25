4f4a08b1dba145251aee78e73f4b7e35c05e92a7
Matheus Abreu <abreumatheus@icloud.com>
2019-11-25 09:55:01 -0300

    Resolução Módulo 3 / Ex 3

--- 3-Modulo_Print-2/matheusabreu/ex3.py
+++ 3-Modulo_Print-2/matheusabreu/ex3.py
@@ -2,3 +2,8 @@
 #--- Crie uma variável e atribua uma string com o nome de um cargo
 #--- Crie uma variável e atribua um float com o salário
 #--- Imprima as duas variáveis juntamente com uma frase utilizando a função format()
+
+cargo = 'Professor'
+salario = 2.50
+
+print('Cargo: {} \nSalário: R${:.2f}'.format(cargo, salario))
