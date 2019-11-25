c53603e69fea7a95b3c19853f2c7e0f6e7d9f4cd
Matheus Abreu <abreumatheus@icloud.com>
2019-11-25 09:51:52 -0300

    Resolução Módulo 3 / Ex 1

--- 3-Modulo_Print-2/matheusabreu/ex1.py
+++ 3-Modulo_Print-2/matheusabreu/ex1.py
@@ -4,3 +4,9 @@
 #--- Crie uma variável e atribua um valor inteiro com sua idade
 #--- Imprima as três variáveis juntamente um uma frase de apresentação
 #--- Utilize a função format()
+
+nome = 'Matheus'
+sobrenome = 'Abreu'
+idade = 21
+
+print(f'Meu nome é {nome} {sobrenome}, tenho {idade} anos.')
