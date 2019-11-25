3dae972d142d3635072c19727b3e9d6922e8582d
gweingaertner <guilherme.weingaertner@gmail.com>
2019-11-25 09:28:11 -0300

    resoluçã funçao exercicios

---
+++ 18-Modulo_Funcao-2/guilherme weingaertner/metodo_1.py
@@ -0,0 +1,36 @@
+def vezes(n1,n2):
+    n3 = n1*n2
+    return n3
+
+def potencia(n1,n2):
+    return(n1**n2)
+
+def cabecalho(n2):
+    return n2*80
+
+def imposto(salario):
+    if salario < 1000.00:
+        n7 = (salario*0.01)
+    elif salario < 3000.00:
+        n7 = (salario*0.015) 
+    elif salario < 6000.00:
+        n7 = (salario*0.02)
+    else:
+        n7 = (salario*0.025)
+
+    return n7
+
+def sair():
+    return print('0-Usuário realizou o logoff')
+    
+def cadastrar():
+   return print('1-Cadastro de usuários')
+
+def listar():
+   return print('2-Lista de usuários cadastrados')
+
+
+    
+
+
+    
