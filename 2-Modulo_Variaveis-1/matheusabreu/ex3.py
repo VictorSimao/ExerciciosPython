667bbbe1bde1a15735c6ea7fedbfd5d9c91b7fb1
Matheus Abreu <abreumatheus@icloud.com>
2019-11-25 09:06:47 -0300

    Resolução Módulo 2 / Ex 3

--- 2-Modulo_Variaveis-1/matheusabreu/ex3.py
+++ 2-Modulo_Variaveis-1/matheusabreu/ex3.py
@@ -7,3 +7,20 @@
 #---    Autor
 #---    Data de publicação
 #--- Os parágrafos devem estar formatados conforme a formatação do livro
+
+titulo = 'Quarto de Despejo'
+edicao = 'Edição Popular'
+autor = 'Carolina Maria de Jesus'
+data = '1960'
+
+print(f'\nTítulo: {titulo} \nEdição: {edicao} \nAutora: {autor} \nData: {data}\n')
+print('''        31 DE DEZEMBRO ...Levantei as 3 e meia e fui carregar
+agua. Despertei os filhos, eles tomaram café. Saímos. O João foi
+catando papel porque quer dinheiro para ir no cinema. que supli-
+cio carregar 3 sacos de papeis. Ganhamos 80 cruzeiros. Dei 30 ao João.
+        Espero que 1960 seja melhor do que 1959. Sofremos tanto
+no 1959, que dá pra gente dizer:
+                Vai, vai mesmo!
+                Eu não quero você mais.
+                Nunca mais!
+        1 DE JANEIRO DE 1960 Levantei as 5 horas e fui carregar agua.\n''')
