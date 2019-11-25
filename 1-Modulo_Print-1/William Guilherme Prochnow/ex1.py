a8f552841d4211a6e1d2b0eff5b0a0ce0410a47c
William Guilherme Prochnow <w.g.p6calmo@gmail.com>
2019-11-25 11:29:03 -0300

    modulo1-py

---
+++ 1-Modulo_Print-1/William Guilherme Prochnow/ex1.py
@@ -0,0 +1,11 @@
+#--- Exercício 1 - Impressão de dados com a função Print
+#--- Imprima textos com seu nome, sobrenome e idade 
+#--- Cada informação deve ser impressa em uma linha diferente 
+
+nome = ('Wiliam Guilhrme')
+sobrenome = ('Prochnow')
+idade = ('17')
+
+print(nome)
+print(sobrenome)
+print(idade)
---
+++ 1-Modulo_Print-1/William Guilherme Prochnow/ex2.py
@@ -0,0 +1,17 @@
+#--- Exercício 2 - Impressão de dados com a função Print
+#--- Imprima textos que simulem o menu de uma aplicação
+#--- A aplicação será de cadastro de pessoas
+#--- O menu deve conter as opções de Cadastrar, Alterar, Listar e Sair
+#--- Cada opção deve vir acompanhada de um número
+#--- Cada opção deve ser impressa em uma linha diferente
+
+cadastro = input('Digite seu cadastro:')
+
+cadastrar = ('1-'' ''Cadastrar')
+print(cadastrar)
+alterar = ('2-'' ''Alterar')
+print(alterar)
+listar = ('2-'' ''Listar')
+print(listar)
+sair = ('3-'' ''sair')
+print(sair)
