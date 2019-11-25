a970c5c99489788c6dd1b42ef5baaf915580cb33
Mateus_Alves <alvesdasilva1106@gmail.com>
2019-11-24 22:54:25 -0300

    Resolvendo exercicios Funções

--- 1-Modulo_Print-1/Mateus_Alves/16-Modulo_Funcao-1/ex3.py
+++ 1-Modulo_Print-1/Mateus_Alves/16-Modulo_Funcao-1/ex3.py
@@ -4,3 +4,13 @@
 #--- Calcule a média entre os três números e armazene em uma quarta variável
 #--- Imprima a média e uma mensagem usando f-string (módulo 3)
 #--- Deve ser impresso apenas duas cadas após a vírgula
+
+n1 = float(input('Informe o Primeiro Numero'))
+n2 = float(input('Infore o Segundo Numero'))
+n3 = float(input('informe o terceiro Numero'))
+
+def media(n1,n2,n3):
+    media = (n2+n2+n3)/3
+    return media
+
+print(f'A media do Aluno foi de {round(media(n1,n2,n3),2)} ')
