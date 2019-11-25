7e49fe1d957ef4fcc471c34325a3f0c30ef47707
G.Gabriel <geoquimics@gmail.com>
2019-11-24 23:11:29 -0300

    modulo 16 - exercicio 03 resolvido

--- 16-Modulo_Funcao-1/ex3.py
+++ 16-Modulo_Funcao-1/ex3.py
@@ -4,3 +4,18 @@
 #--- Calcule a média entre os três números e armazene em uma quarta variável
 #--- Imprima a média e uma mensagem usando f-string (módulo 3)
 #--- Deve ser impresso apenas duas cadas após a vírgula
+
+a = ''
+b = ''
+c = ''
+
+def media (a,b,c):
+    a = float(input('Digite a: '))
+    b = float(input('Digite b: '))
+    c = float(input('Digite c: '))
+    media = (a + b + c) / 3
+    return media
+
+media = media(a,b,c)
+
+print(f'A média de a + b + c é {media :.2f}')
