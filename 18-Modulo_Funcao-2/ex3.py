0e633ba33dc4a616725737176904243c2b24c51c
Matheus Abreu <abreumatheus@icloud.com>
2019-11-24 22:13:50 -0200

    Resolução Módulo 18 / Ex 3

--- 18-Modulo_Funcao-2/ex3.py
+++ 18-Modulo_Funcao-2/ex3.py
@@ -5,3 +5,12 @@
 #--- O segundo dado deve ser o caracter que será multiplicado para fazer a linha de cabeçalho
 #--- A impressão deve ser realizada utilizando f-string
 #--- A função deve ser chamada informando os dois dados
+
+def header(sistema, char):
+    print(f'{char*55}\n{sistema}\n{char*55}\n')
+    return ''
+
+sistema = '\nHB\n'
+char = '#'
+
+header(sistema, char)
