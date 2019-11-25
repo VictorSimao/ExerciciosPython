776df9c780550d53e3762a2562fce5c896720f60
Matheus Abreu <abreumatheus@icloud.com>
2019-11-25 09:53:12 -0300

    Resolução Módulo 3 / Ex 2

--- 3-Modulo_Print-2/matheusabreu/ex2.py
+++ 3-Modulo_Print-2/matheusabreu/ex2.py
@@ -2,3 +2,8 @@
 #--- Crie uma variável e atribua uma string com seu nome completo
 #--- Crie uma variável e atribua um valor de ponto flutuante com sua altura
 #--- Imprima as duas variáveis juntamente com uma frase utilizando f-string
+
+nome_completo = 'Matheus Abreu'
+altura = 1.73
+
+print(f'Meu nome é {nome_completo}, tneho {altura}m de altura.')
