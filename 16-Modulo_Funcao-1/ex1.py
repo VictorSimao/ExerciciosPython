e84ec5bce4aa8dff56d817b766434b99969c722c
PabloSchumacher <pabloschumacherropelato@gmail.com>
2019-11-25 00:24:46 -0300

    Resolução dos Exercicios / Módulos 1,2,16 e 18(ex 1)

--- 16-Modulo_Funcao-1/ex1.py
+++ 16-Modulo_Funcao-1/ex1.py
@@ -3,3 +3,12 @@
 #--- O cabeçalho deve ser escrito usando a multiplicação de carácter
 #--- O cabeçalho deev conter o nome de uma empresa, que será uma variável
 #--- Realize a chamada da função na ultima linha do seu programa
+
+nome = 'HBSIS'
+
+def cabecalho(nome):
+    print('='*30,'\n')
+    print(nome,'\n')
+    print('='*30)
+    return nome
+cabecalho(nome)
--- 16-Modulo_Funcao-1/ex2.py
+++ 16-Modulo_Funcao-1/ex2.py
@@ -3,3 +3,13 @@
 #--- Armazene cada número em uma variável
 #--- Realize a soma entre os dois números e armazene o resultado em uma terceira variável
 #--- Imprima o resultado e uma mensagem usando f-string (módulo 3)
+
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+
+def calc(n1,n2):
+    r = (n1+n2)
+    return r
+
+rcalc = calc(n1,n2)
+print(f'A soma entre {n1} e {n2} é de {rcalc}')
