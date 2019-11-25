d9c86ba06216be6ea691b2bfcd363b77bb74aae5
Matheus Abreu <abreumatheus@icloud.com>
2019-11-25 08:21:15 -0300

    Resolução Módulo 1

--- 1-Modulo_Print-1/ex2.py
+++ 1-Modulo_Print-1/ex2.py
@@ -4,3 +4,4 @@
 #--- O menu deve conter as opções de Cadastrar, Alterar, Listar e Sair
 #--- Cada opção deve vir acompanhada de um número
 #--- Cada opção deve ser impressa em uma linha diferente
+print('MENU: \n1 -- Cadastrar\n2 -- Alterar\n3 -- Listar Pessoas\n4 -- Sair')
