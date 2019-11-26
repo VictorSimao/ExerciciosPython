6a5058338e83a0569b05a395fc92b532e8cb9799
William Guilherme Prochnow <w.g.p6calmo@gmail.com>
2019-11-26 11:16:52 -0300

    modulo2

---
+++ 2-Modulo_Variavel-1/William Guilherme Prochnow/modulo2-3.py
@@ -0,0 +1,24 @@
+#--- Exercício 3  - Variáveis
+#--- Imprima dois parágrafos do último livro que você leu
+#--- A impressão deve conter informações do livro, que deverão estar em variáveis
+#--- As informações do Livro serão: 
+#---    Título
+#---    Edição
+#---    Autor
+#---    Data de publicação
+#--- Os parágrafos devem estar formatados conforme a formatação do livro
+
+paragrafos = ('Sábado, 15 de julho de 1944\n\tQuerida Kitty,\n\t\tRecebemos da biblioteca um livro com o título polêmico: O\n\tque você acha da jovem moderna? Gostaria de tratar desse assunto hoje.\n\t\tA escritora critica a "juventude atual" da cabeça aos pés, ainda\n\tque não condene todos como "casos sem esperança", pelo contrá-\n\trio, ela acredita que os jovens têm o poder de construir um mundo\n\tmaior, melhor e mais belo, mas que se ocupam com coisas supe-\n\tficiais, sem pensar na beleza verdadeira.')
+print(paragrafos)
+print('\n')
+titulo = 'Titulo: O diário de ANNE FRANK'
+print(titulo)
+print('\n')
+editora = 'Editora: Pe Da Letra'
+print(editora)
+print('\n')
+autor = 'Autora: Anne Frank'
+print(autor)
+print('\n')
+data = 'Data de Publicação: 1ª (20 de novembro de 2019)'
+print(data)
---
+++ 2-Modulo_Variavel-1/William Guilherme Prochnow/modulo2-5.py
@@ -0,0 +1,27 @@
+#--- Exercício 5  - Variáveis
+#--- Imprima os dados de 5 papéis cotados na bolsa de valores de SP
+#--- Cada Papel apresentado deve possuir: 
+#---    Nome 
+#---    Tipo
+#---    Cotação Atual
+#---    Valor Mínimo do dia 
+#---    Valor Máximo do dia 
+#--- Os dados dos papéis devem estar em variáveis
+#--- A tela deve conter cabeçalho e rodapé
+#--- Deve ser usado os caracteres de tabulação e quebra de linha
+
+print('='*50)
+papel1 = ('Papel 1:\n\tNome: JBS ON\n\tTipo: Ação\n\tCotação Atual: R$ 28,29\n\tValor mínimo do dia: R$ 27,34\n\tValor máximo do dia: R$ 28,40')
+papel2 = ('Papel 2:\n\tNome: BRF FOODS ON\n\tTipo: Ação\n\tCotação Atual: R$ 36,73\n\tValor mínimo do dia: R$ 36,12\n\tValor máximo do dia: R$ 36,80')
+papel3 = ('Papel 3:\n\tNome: SID NACIONAL ON\n\tTipo: Ação\n\tCotação Atual: R$ 12,53\n\tValor mínimo do dia: R$ 12,22\n\tValor máximo do dia: R$ 12,55')
+papel4 = ('Papel 4:\n\tNome: MARFRIG ON\n\tTipo: Ação\n\tCotação Atual: R$ 11,51\n\tValor mínimo do dia: R$ 10,88\n\tValor máximo do dia: R$ 11,53')
+papel5 = ('Papel 5:\n\tNome: KLABIN UNT\n\tTipo: Ação\n\tCotação Atual: R$ 17,63\n\tValor mínimo do dia: R$ 17,21\n\tValor máximo do dia: R$ 17,64')
+print(papel1)
+print(papel2)
+print(papel3)
+print(papel4)
+print(papel5)
+print('='*50)
+
+
+
