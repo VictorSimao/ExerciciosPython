219e0be1f78cd60b32ec544551da95ec84cd4453
unknown <900136@proway.treina>
2019-11-26 12:00:57 -0300

    Exercicos ate o modulo 18

--- 16-Modulo_Funcao-1/Resolucao/ex2.py
+++ 16-Modulo_Funcao-1/Resolucao/ex2.py
@@ -4,10 +4,9 @@
 #--- Realize a soma entre os dois números e armazene o resultado em uma terceira variável
 #--- Imprima o resultado e uma mensagem usando f-string (módulo 3)
 
-def calcular(n1,n2):
-    return n1+n2
-
-a=int(input('digite n1: '))
-b=int(input('digite n2: '))
-
-print(f'O resultado é {calcular(a,b)}')
+def calcular ():
+    n1=int(input('digite um numero: '))
+    n2=int(input('digite outro numero: '))
+    res=n1+n2
+    print(f'o resultado de {n1} + {n2} é = {res}')
+calcular ()
---
+++ 16-Modulo_Funcao-1/Resolucao/ex3.py
@@ -0,0 +1,14 @@
+#--- Exercício 3  - Funções - 1
+#--- Crie uma função que leia três números float
+#--- Armazene cada valor lido em uma variável
+#--- Calcule a média entre os três números e armazene em uma quarta variável
+#--- Imprima a média e uma mensagem usando f-string (módulo 3)
+#--- Deve ser impresso apenas duas cadas após a vírgula
+
+def calcula_media():
+    n1=float(input('Digite n1: '))
+    n2=float(input('Digite n2: '))
+    n3=float(input('Digite n3: '))
+    media=((n1+n2+n3)/3)
+    print(f'A media de {n1} + {n2} + {n3} é = {media}')
+calcula_media ()
